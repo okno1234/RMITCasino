4e4b38671285f1020709d1a6efd5c77c9a6b2742
Eduardo Salcedo <s4118015@rmit.edu.vn>
2024-08-17 21:43:51 +0800

    Initial Commit

---
+++ RMITCasino/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RMITCasino
+//
+//  Created by user264150 on 8/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RMITCasino/RMITCasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMITCasinoApp.swift
+//  RMITCasino
+//
+//  Created by user264150 on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct RMITCasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
